6208bc0c14c08c689a7bdf904d039313961f3e0f
Clayton Liew <72314373+clayton140450@users.noreply.github.com>
2020-12-19 13:06:46 +0000

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,6 @@
+export const size = 50;
+export const enemies = [];
+export const canvas = document.getElementById("canvas");
+export const ctx = canvas.getContext("2d");
+canvas.width = 1600;
+canvas.height = 900;
